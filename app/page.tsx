035dbce8235f4635673010f1a8449c28c8d6a2f5
brianctb <brianlee1188@gmail.com>
2025-07-03 17:38:04 +0800

    Refactor code and add a project route

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,19 @@
 import { BlogPosts } from 'app/components/posts'
 
-export default function Page() {
-  return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
+const metadata = {
+  title: 'My Portfolio',
+  intro: `I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
         Vim's keystroke commands and tabs' flexibility for personal viewing
         preferences. This extends to my support for static typing, where its
         early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
+        mode, which eases long coding sessions by reducing eye strain.`
+}
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="section-title">{metadata.title}</h1>
+      <p className="mb-4">{metadata.intro}</p>
       <div className="my-8">
         <BlogPosts />
       </div>
---
+++ app/project/page.tsx
@@ -0,0 +1,11 @@
+const metadata = {
+    title: 'My Portfolio',
+}
+
+export default function Page() {
+    return (
+        <section>
+            <h1 className="section-title">{metadata.title}</h1>
+        </section>
+    )
+}
