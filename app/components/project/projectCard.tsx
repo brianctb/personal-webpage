871ee38454d01b92f4ed20f88387ecc59c6d9d6f
brianctb <brianlee1188@gmail.com>
2025-07-07 08:56:57 +0800

    add project card features

--- app/components/project/projectCard.tsx
+++ app/components/project/projectCard.tsx
@@ -8,31 +8,65 @@ export const ProjectCard = ({ project }: { project: Project }) => {
     const [isExpanded, setIsExpanded] = useState(false);
 
     return (
-        <div className="mb-4 border-b border-neutral-200 dark:border-neutral-800 pb-4">
+        <div className="mb-4 pb-4">
+            <div className="flex flex-col items-center justify-center">
+                <div className="flex w-full">
+                    <div className="flex-1 flex justify-center">
+                        <img
+                            src={`/image/${project.imagePath}`}
+                            alt={project.slug}
+                            className="w-30 h-20"
+                        />
+                    </div>
+                    <div className="flex-1 flex justify-center items-center">
+                        <div className="flex flex-col items-center">
+                            <p className="text-xl text-center text-neutral-900 dark:text-neutral-100 tracking-tight font-medium">
+                                {project.slug}
+                            </p>
+                            <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
+                                {formatDate(project.publishedAt, false)}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                {isExpanded && (
+                    <div className="w-full mt-2 flex flex-col items-center">
+                        <p className="mt-2 w-full text-xl text-left lg:text-center text-neutral-300">
+                            {"Description"}
+                        </p>
+                        <p className="w-full text-left lg:text-center text-neutral-400">
+                            {project.description}
+                        </p>
+                        <p className="mt-2 w-full text-xl text-left lg:text-center text-neutral-300">
+                            {"Technologies"}
+                        </p>
+                        <div className="mt-2 flex flex-wrap gap-2 lg:justify-center">
+                            {project.techStack.map((tech) => (
+                                <span
+                                    key={tech}
+                                    className="px-4 py-2 rounded-full font-medium bg-neutral-700 text-neutral-400  min-w-[100px] text-center"
+                                >
+                                    {tech}
+                                </span>
+                            ))}
+                        </div>
+                        <p className="mt-2 w-full text-xl text-left lg:text-center text-neutral-700 dark:text-neutral-300">
+                            {"Duties"}
+                        </p>
+                        <ul className="list-disc list-inside text-neutral-400 w-full text-left lg:text-center">
+                            {project.duties.map((duty, index) => (
+                                <li key={index}>{duty}</li>
+                            ))}
+                        </ul>
+                    </div>
+                )}
+            </div>
             <button
-                className="w-full flex flex-col md:flex-row items-start md:items-center space-x-0 md:space-x-2 text-left"
+                className="flex items-center justify-center w-full"
                 onClick={() => setIsExpanded(!isExpanded)}
             >
-                <span className="mr-2 transition-transform duration-200">
-                    {isExpanded ? '▲' : '▼'}
-                </span>
+                <span>{isExpanded ? '▲' : '▼'}</span>
             </button>
-            <div className="flex items-center gap-x-2">
-                <p className="text-neutral-900 dark:text-neutral-100 tracking-tight font-medium">
-                    {project.slug}
-                </p>
-                <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                    {formatDate(project.publishedAt, false)}
-                </p>
-            </div>
-
-            {isExpanded && (
-                <div className="mt-2 pl-6 md:pl-8">
-                    <p className="text-neutral-700 dark:text-neutral-300">
-                        {project.shortDescription}
-                    </p>
-                </div>
-            )}
         </div>
     );
 };
--- app/interface/project.ts
+++ app/interface/project.ts
@@ -1,29 +1,29 @@
 export interface Project {
   slug: string;
-  shortDescription: string;
   description: string;
   techStack: string[];
-  duties: string;
+  duties: string[];
   publishedAt: string;
   link?: string;
+  imagePath?: string;
 }
 
 export const projects: Project[] = [
   {
     slug: "AI Storyteller",
-    shortDescription: "A web application that generates stories using AI.",
     description: "Placeholder for AI Storyteller project description.",
     publishedAt: "2025-04-30",
-    duties: "Developed a web application that generates stories using AI.",
+    duties: ["Developed a web application that generates stories using AI."],
     techStack: ["JavaScript", "Node.js", "Express", "PostgreSQL", "OpenAI API"],
+    imagePath: "question_mark.png",
   },
   {
     slug: "Canada Employment Visualization",
-    shortDescription: "A data visualization tool to analyze employment trends in Canada.",
     description: "Placeholder for Canada Employment Visualization project description.",
     publishedAt: "2023-06-15",
-    duties: "Created a data visualization tool to analyze employment trends in Canada.",
+    duties: ["Created a data visualization tool to analyze employment trends in Canada."],
     techStack: ["JavaScript", "Python","React", "Node.js", "MongoDB", "Chart.js"],
-    link: "https://canada-employment-visualization.vercel.app"
+    link: "https://canada-employment-visualization.vercel.app",
+    imagePath: "question_mark.png"
   }
 ];
