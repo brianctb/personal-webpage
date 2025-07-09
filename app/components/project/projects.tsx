fa39f4ff73221d31b74eecf22a050f2062cece50
brianctb <brianlee1188@gmail.com>
2025-07-09 13:21:28 +0800

    remove deprecated code

--- app/components/project/projects.tsx
+++ app/components/project/projects.tsx
@@ -12,16 +12,6 @@ export const ProjectPosts = () => {
                 }
                 return 1;
             }).map((projectData) => (
-                // <div key={project.slug} className="flex flex-col space-y-1 mb-4">
-                //     <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-                //         <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                //             {formatDate(project.publishedAt, false)}
-                //         </p>
-                //         <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                //             {project.slug}
-                //         </p>
-                //     </div>
-                // </div>
                 <ProjectCard key={projectData.slug} project={projectData} />
             ))
             }
