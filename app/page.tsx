f0c9a0fca0e3797b8ef12113db24f5bb713bfb6e
brianctb <brianlee1188@gmail.com>
2025-07-11 09:54:13 +0800

    change formatting on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { introduction, workExperiences, educations, skills, interests } from "./interface/homepage";
 import { PillGroup } from "./components/pillGroup";
 import React from "react";
-
+import { formatDate } from "./utils/date/format";
 
 export default function HomePage() {
 
@@ -21,8 +21,8 @@ export default function HomePage() {
     "bg-purple-500/80"
   ];
 
-  const [skillExpanded, setSkillExpanded] = React.useState(true);
-  const [workExpanded, setWorkExpanded] = React.useState(true);
+  const [skillExpanded, setSkillExpanded] = React.useState(false);
+  const [workExpanded, setWorkExpanded] = React.useState(false);
 
   return (
     <section className="flex flex-col">
@@ -51,7 +51,7 @@ export default function HomePage() {
                 key={index}
                 className="bg-neutral-900/70 p-3 rounded-2xl flex flex-col items-center"
               >
-                <h2 className="font-bold">{skill.genre}</h2>
+                <h2 className="font-bold text-lg">{skill.genre}</h2>
                 <PillGroup items={skill.skills} pillClassName={skillPillColorScheme[index]} className="justify-center" />
               </div>
             ))}
@@ -66,43 +66,50 @@ export default function HomePage() {
           </button>
         </div>
         {workExpanded && (
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
-            {workExperiences.map((experience, index) => (
-              <div key={index} className="bg-neutral-900/70 p-3 rounded-2xl">
-                <h2 className="font-bold">{experience.position} at {experience.company}</h2>
-                <p className="text-neutral-400">{experience.startDate} - {experience.endDate}</p>
-                {experience.duties && (
-                  <ul className="list-disc list-inside text-neutral-400 mt-2">
-                    {experience.duties.map((duty, dutyIndex) => (
-                      <li key={dutyIndex}>{duty}</li>
-                    ))}
-                  </ul>
-                )}
-              </div>
-            ))}
+          <div className="grid gap-4 mt-4">
+            {workExperiences
+              .sort((a, b) => new Date(b.endDate).getTime() - new Date(a.endDate).getTime())
+              .map((experience, index) => (
+                <div key={index} className="bg-neutral-900/70 p-5 rounded-2xl">
+                  <h2 className="font-bold text-xl">{experience.position}</h2>
+                  <h2 className='font-bold text-lg'>{experience.company}</h2>
+                  <p className="text-neutral-400">{formatDate(experience.startDate)} - {formatDate(experience.endDate)}</p>
+                  {experience.duties && (
+                    <ul className="list-disc list-inside text-neutral-400 mt-2 text-lg">
+                      {experience.duties.map((duty, dutyIndex) => (
+                        <li key={dutyIndex} className="mb-3">
+                          {duty}
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </div>
+              ))}
           </div>
         )}
       </section>
       <section className="mb-8">
         <h1 className="section-title">Education</h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
-          {educations.map((education, index) => (
-            <div key={index} className="bg-neutral-900/70 p-3 rounded-2xl">
-              <h2 className="font-bold">{education.degree}</h2>
-              <p className="text-neutral-400">{education.institution}</p>
-              <p className="text-neutral-400">{education.startDate} - {education.endDate}</p>
-              {education.GPA && (
-                <p className="text-neutral-400">GPA: {education.GPA}</p>
-              )}
-            </div>
-          ))}
+          {educations
+            .sort((a, b) => new Date(b.endDate).getTime() - new Date(a.endDate).getTime())
+            .map((education, index) => (
+              <div key={index} className="bg-neutral-900/70 p-3 rounded-2xl">
+                <h2 className="font-bold text-lg">{education.degree}</h2>
+                <p className="text-neutral-400 text-lg">{education.institution}</p>
+                <p className="text-neutral-400">{formatDate(education.startDate)} - {formatDate(education.endDate)}</p>
+                {education.GPA && (
+                  <p className="text-neutral-400">GPA: {education.GPA}</p>
+                )}
+              </div>
+            ))}
         </div>
       </section>
       <section className="mb-8">
         <h1 className="section-title">Interests</h1>
         <button
           className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-4 gap-3"
-
+          onClick={() => { }}
         >
           {interests.map((interest, index) => (
             <div key={index} className={`p-3 rounded-2xl flex items-center justify-center ${interestPillColorScheme[index]}`}>
