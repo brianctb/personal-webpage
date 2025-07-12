3423940dda331abd674ff95377d9b4ed7a43044d
brianctb <brianlee1188@gmail.com>
2025-07-12 12:37:01 +0800

    add blog post for caracal

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -84,14 +84,14 @@ export default async function Blog({ params }) {
       <h1 className="title font-semibold text-2xl tracking-tighter">
         {post.metadata.title}
       </h1>
-      <div className="flex justify-between items-center mt-2 mb-8 text-sm">
+      <div className="flex justify-between items-center mt-2 mb-5 text-sm">
         <p className="text-sm text-neutral-600 dark:text-neutral-400">
           {formatDate(post.metadata.publishedAt)}
         </p>
       </div>
       {post.metadata.technologies?.length > 0 && (
         <>
-          <p className="title font-semibold text-2xl mb-4">
+          <p className="title font-semibold text-xl mb-4">
             <span>Technologies</span>
           </p>
           <PillGroup
