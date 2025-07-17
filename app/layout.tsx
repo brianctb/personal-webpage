f38b0956a89eb91b86b365478dff3b8031b3f691
brianctb <brianlee1188@gmail.com>
2025-07-17 09:25:02 +0800

    change fields in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,10 @@ export const metadata: Metadata = {
   },
   description: 'This is my portfolio.',
   openGraph: {
-    title: 'My Portfolio',
+    title: 'Brian\'s Portfolio',
     description: 'This is my portfolio.',
     url: baseUrl,
-    siteName: 'My Portfolio',
+    siteName: 'Brian\'s Portfolio',
     locale: 'en_US',
     type: 'website',
   },
